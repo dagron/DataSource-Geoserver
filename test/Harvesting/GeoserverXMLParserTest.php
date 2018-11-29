63fdb220584511d37c29d966417fc4ffe96a2068
Willem ter Berg <wrpterberg@gmail.com>
2018-11-29 14:05:27 +0100

    xml parser tests

--- test/Harvesting/GeoserverXMLParserTest.php
+++ test/Harvesting/GeoserverXMLParserTest.php
@@ -9,8 +9,61 @@ class GeoserverXMLParserTest extends TestCase
 {
     public function testReturnsEmptyListWhenNoEntitiesAreFound(): void
     {
-        $parser = new GeoserverXMLParser(new \SimpleXMLElement('<root></root>'));
+        $parser = new GeoserverXMLParser(new \SimpleXMLElement('
+            <root>
+            </root>
+        '));
 
         $this->assertSame([], $parser->getAllEntities());
+    }
+
+    public function testReturnsAllTheChildrenOfFeatureTypeList(): void
+    {
+        $parser = new GeoserverXMLParser(new \SimpleXMLElement('
+            <root>
+                <FeatureTypeList>
+                    <a></a>
+                    <b></b>
+                </FeatureTypeList>
+            </root>
+        '));
+
+        $this->assertTrue(\count($parser->getAllEntities()) === 2);
+    }
+
+    public function testReturnsEmptyStringWhenNoTitleIsFound(): void
+    {
+        $parser = new GeoserverXMLParser(new \SimpleXMLElement('<root></root>'));
+
+        $this->assertSame('', $parser->findTitle());
+    }
+
+    public function testReturnsTitleWhenTitleIsFound(): void
+    {
+        $parser = new GeoserverXMLParser(new \SimpleXMLElement('
+            <root>
+                <Title>test</Title>
+            </root>
+        '));
+
+        $this->assertSame('test', $parser->findTitle());
+    }
+
+    public function testReturnsEmptyStringWhenNoAbstractIsFound(): void
+    {
+        $parser = new GeoserverXMLParser(new \SimpleXMLElement('<root></root>'));
+
+        $this->assertSame('', $parser->findAbstract());
+    }
+
+    public function testReturnsAbstractWhenAbstractIsFound(): void
+    {
+        $parser = new GeoserverXMLParser(new \SimpleXMLElement('
+            <root>
+                <Abstract>test</Abstract>
+            </root>
+        '));
+
+        $this->assertSame('test', $parser->findAbstract());
     }
 }
