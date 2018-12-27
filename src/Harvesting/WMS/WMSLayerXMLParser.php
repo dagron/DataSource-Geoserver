7a903c271b2715f235c268bf7ab37e6e67dbf080
Willem ter Berg <wrpterberg@gmail.com>
2018-12-27 16:08:01 +0100

    additional unittests for WMS harvesting

--- src/Harvesting/WMS/WMSLayerXMLParser.php
+++ src/Harvesting/WMS/WMSLayerXMLParser.php
@@ -63,14 +63,16 @@ class WMSLayerXMLParser extends XMLParser
         }
 
         foreach ($this->xml->BoundingBox as $box) {
-            if ('EPSG:28992' == $box->attributes()['SRS']) {
-                $min_x = \substr($box->attributes()['minx'], 0, 6);
-                $min_y = \substr($box->attributes()['miny'], 0, 6);
-                $max_x = \substr($box->attributes()['maxx'], 0, 6);
-                $max_y = \substr($box->attributes()['maxy'], 0, 6);
-
-                return \sprintf('%s,%s,%s,%s', $min_x, $min_y, $max_x, $max_y);
+            if ('EPSG:28992' != $box->attributes()['SRS']) {
+                continue;
             }
+
+            $min_x = \substr($box->attributes()['minx'], 0, 6);
+            $min_y = \substr($box->attributes()['miny'], 0, 6);
+            $max_x = \substr($box->attributes()['maxx'], 0, 6);
+            $max_y = \substr($box->attributes()['maxy'], 0, 6);
+
+            return \sprintf('%s,%s,%s,%s', $min_x, $min_y, $max_x, $max_y);
         }
 
         return '';
--- test/Harvesting/WMS/WMSResponseXMLParserTest.php
+++ test/Harvesting/WMS/WMSResponseXMLParserTest.php
@@ -2,12 +2,205 @@
 
 namespace NijmegenSync\Test\DataSource\Geoserver\Harvesting\WMS;
 
+use NijmegenSync\DataSource\Geoserver\Harvesting\WMS\WMSResponseXMLParser;
 use PHPUnit\Framework\TestCase;
 
 class WMSResponseXMLParserTest extends TestCase
 {
-    public function testPlaceholder(): void
+    public function testReturnsEmptyArrayWhenNoQueryableLayersAreFound(): void
     {
-        $this->assertTrue(true);
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <Layer />
+            </root>
+        '));
+
+        $this->assertTrue(0 == \count($parser->getAllLayers()));
+    }
+
+    public function testReturnsLayerParsersForEachQueryableLayer(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <Layer />
+                <Layer queryable="1">
+                    <someNode />
+                </Layer>
+                <Layer queryable="1">
+                    <someOtherNode />
+                </Layer>
+                <Layer />
+            </root>
+        '));
+
+        $this->assertTrue(2 == \count($parser->getAllLayers()));
+    }
+
+    public function testReturnsEmptyStringWhenNoContactPersonIsFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <someNode />
+                    </ContactPersonPrimary>
+                </ContactInformation>
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findContactPerson());
+    }
+
+    public function testReturnsContactPersonWhenFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <ContactPerson>test</ContactPerson>
+                    </ContactPersonPrimary>
+                </ContactInformation>
+            </root>
+        '));
+
+        $this->assertSame('test', $parser->findContactPerson());
+    }
+
+    public function testReturnsEmptyStringWhenNoContactOrganizationIsFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <someNode />
+                    </ContactPersonPrimary>
+                </ContactInformation>
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findContactOrganization());
+    }
+
+    public function testReturnsContactOrganizationWhenFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <ContactOrganization>test</ContactOrganization>
+                    </ContactPersonPrimary>
+                </ContactInformation>
+            </root>
+        '));
+
+        $this->assertSame('test', $parser->findContactOrganization());
+    }
+
+    public function testReturnsEmptyStringWhenNoContactEmailIsFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <someNode />
+                    </ContactPersonPrimary>
+                </ContactInformation>
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findContactEmail());
+    }
+
+    public function testReturnsContactEmailWhenFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactElectronicMailAddress>test@test.nl</ContactElectronicMailAddress>
+                </ContactInformation>
+            </root>
+        '));
+
+        $this->assertSame('test@test.nl', $parser->findContactEmail());
+    }
+
+    public function testReturnsEmptyStringWhenNoAccessConstraintIsFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <someNode />
+                    </ContactPersonPrimary>
+                </ContactInformation>
+                <Service />
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findAccessRights());
+    }
+
+    public function testReturnsAccessConstraintWhenFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <Service>
+                    <AccessConstraints>NONE</AccessConstraints>
+                </Service>
+            </root>
+        '));
+
+        $this->assertSame('NONE', $parser->findAccessRights());
+    }
+
+    public function testReturnsEmptyStringWhenNoOutputFormatIsFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <ContactInformation>
+                    <ContactPersonPrimary>
+                        <someNode />
+                    </ContactPersonPrimary>
+                </ContactInformation>
+                <Service />
+                <Capability />
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findDesiredOutputFormat());
+    }
+
+    public function testReturnsPNGWhenFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <Capability>
+                    <Request>
+                        <GetMap>
+                            <Format>image/png</Format>
+                        </GetMap>
+                    </Request>
+                </Capability>
+            </root>
+        '));
+
+        $this->assertSame('image/png', $parser->findDesiredOutputFormat());
+    }
+
+    public function testReturnsJPGWhenNoPNGIsFound(): void
+    {
+        $parser = new WMSResponseXMLParser(new \SimpleXMLElement('
+            <root>
+                <Capability>
+                    <Request>
+                        <GetMap>
+                            <Format>image/jpeg</Format>
+                        </GetMap>
+                    </Request>
+                </Capability>
+            </root>
+        '));
+
+        $this->assertSame('image/jpeg', $parser->findDesiredOutputFormat());
     }
 }
