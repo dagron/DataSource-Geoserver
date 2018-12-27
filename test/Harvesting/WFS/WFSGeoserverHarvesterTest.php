97e4e92ced4d9839911b5699085aaeaaa155e13e
Willem ter Berg <wrpterberg@gmail.com>
2018-12-27 14:14:41 +0100

    unittests for getters and setters of the WFSGeoserverHarvester

--- test/Harvesting/WFS/WFSGeoserverHarvesterTest.php
+++ test/Harvesting/WFS/WFSGeoserverHarvesterTest.php
@@ -2,12 +2,30 @@
 
 namespace NijmegenSync\Test\DataSource\Geoserver\Harvesting\WFS;
 
+use NijmegenSync\DataSource\Geoserver\Harvesting\WFS\WFSGeoserverHarvester;
 use PHPUnit\Framework\TestCase;
 
 class WFSGeoserverHarvesterTest extends TestCase
 {
-    public function testPlaceholder(): void
+    public function testBaseURLGettingAndSetting(): void
     {
-        $this->assertTrue(true);
+        $harvester = new WFSGeoserverHarvester();
+        $harvester->setBaseUrl('https://my.test.url');
+
+        $this->assertSame(
+            'https://my.test.url',
+            $harvester->getBaseUrl()
+        );
+    }
+
+    public function testLayersURIGettingAndSetting(): void
+    {
+        $harvester = new WFSGeoserverHarvester();
+        $harvester->setLayersUri('https://my.test.url');
+
+        $this->assertSame(
+            'https://my.test.url',
+            $harvester->getLayersUri()
+        );
     }
 }
