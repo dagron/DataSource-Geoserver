9f630e6e115c6231a6cfd3a1e0023c33f8a40369
Willem ter Berg <wrpterberg@gmail.com>
2018-12-27 11:25:06 +0100

    additional unit tests covering the WFSResponseXMLParser

--- test/Harvesting/WFS/WFSResponseXMLParserTest.php
+++ test/Harvesting/WFS/WFSResponseXMLParserTest.php
@@ -30,4 +30,117 @@ class WFSResponseXMLParserTest extends TestCase
 
         $this->assertTrue(2 === \count($parser->getAllEntities()));
     }
+
+    public function testReturnsAnEmptyStringWhenNoContactNameCanBeFound(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceProvider>
+                    <ows:ServiceContact />
+                </ows:ServiceProvider>
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findContactName());
+    }
+
+    public function testCanFindADefinedContactName(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceProvider>
+                    <ows:ServiceContact>
+                        <ows:IndividualName>TestName</ows:IndividualName>
+                    </ows:ServiceContact>
+                </ows:ServiceProvider>
+            </root>
+        '));
+
+        $this->assertSame('TestName', $parser->findContactName());
+    }
+
+    public function testReturnsAnEmptyStringWhenNoContactOrganizationCanBeFound(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceProvider />
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findContactOrganization());
+    }
+
+    public function testCanFindADefinedContactOrganization(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceProvider>
+                    <ows:ProviderName>TestName</ows:ProviderName>
+                </ows:ServiceProvider>
+            </root>
+        '));
+
+        $this->assertSame('TestName', $parser->findContactOrganization());
+    }
+
+    public function testReturnsAnEmptyStringWhenNoContactEmailCanBeFound(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceProvider />
+            </root>
+        '));
+
+        $this->assertSame('', $parser->findContactEmail());
+    }
+
+    public function testCanFindADefinedContactEmail(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceProvider>
+                    <ows:ServiceContact>
+                        <ows:ContactInfo>
+                            <ows:Address>
+                                <ows:ElectronicMailAddress>test@test.nl</ows:ElectronicMailAddress>
+                            </ows:Address>
+                        </ows:ContactInfo>
+                    </ows:ServiceContact>
+                </ows:ServiceProvider>
+            </root>
+        '));
+
+        $this->assertSame('test@test.nl', $parser->findContactEmail());
+    }
+
+    public function testReturnsEmptyArrayWhenNoKeywordsAreFound(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:Keywords />
+            </root>
+        '));
+
+        $this->assertTrue(0 == \count($parser->findKeywords()));
+    }
+
+    public function testReturnsTheFoundKeywords(): void
+    {
+        $parser = new WFSResponseXMLParser(new \SimpleXMLElement('
+            <root xmlns:ows="http://www.opengis.net/ows/1.1">
+                <ows:ServiceIdentification>
+                    <ows:Keywords>
+                        <ows:Keyword>test1</ows:Keyword>
+                        <ows:Keyword>test2</ows:Keyword>
+                    </ows:Keywords>      
+                </ows:ServiceIdentification>
+            </root>
+        '));
+
+        $this->assertTrue(2 == \count($parser->findKeywords()));
+        $this->assertSame(
+            ['test1', 'test2'],
+            $parser->findKeywords()
+        );
+    }
 }
